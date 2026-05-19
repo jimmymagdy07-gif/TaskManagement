6ae76258b186eadfab3bcecdcdeb49ae706dc951
jimmy <jimmyjimmmy66@gmail.com>
2026-05-19 13:19:19 +0300

    fix: update baseURL to railway

--- client/src/hooks/useApi.js
+++ client/src/hooks/useApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://taskmanagement-production-93d5.up.railway.app/api',
   withCredentials: true,
   headers: { 'Content-Type': 'application/json' },
 });
